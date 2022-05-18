876b24b50d9a7fce551c20e5bf0a2ed56ffcbaf1
Ali Nuri <ansprogress@gmail.com>
2022-05-18 14:07:16 +0300

    Score project

---
+++ src/Score/Main.java
@@ -0,0 +1,28 @@
+package Score;
+
+
+/* Please Run this program inside Netbeans followin RUN - > Run Configurations -> Arguments section */
+public class Main {
+    public static void main(String[] args){
+        String testscore = args[0]; // array
+        int score = Integer.parseInt(testscore);
+
+        char grade;
+        if (score >= 90 ) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >=60) {
+            grade = 'C';
+        } else {
+            grade = 'F';
+        }
+        System.out.println("Your grade is " + grade);
+
+        if (grade == 'A' || grade == 'B'){
+            System.out.println("\uD83D\uDC4F");
+        }
+
+
+    }
+}
