678f0cbf9b5e121b401b27c28e2c1c71d5f5d4eb
Ali Nuri <ansprogress@gmail.com>
2022-05-23 15:08:39 +0300

    Static -> Citizen project

---
+++ src/Ex_12_Static/Citizen.java
@@ -0,0 +1,66 @@
+package Ex_12_Static;
+
+import java.util.Date;
+
+public class Citizen {
+    int id;
+    String firstName;
+    String lastName;
+    String placeOfBirth;
+    char cinsiyet;
+    String fatherName;
+    String motherName;
+    Date dateOfBirth;
+
+
+
+    public Citizen(int id, String firstName, String lastName){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    //declaration static variable
+    static String nationality;
+    //getter
+    static String getNationality(){
+        return nationality;
+    }
+
+
+
+
+
+    public static void main(String[] args){
+        Citizen citizen1 = new Citizen(52, "AliNuri", "Amantay");
+        Citizen citizen2 = new Citizen(43, "Mustafa", "Yildirim");
+
+        // мы тут обращаемся к КЛАССУ, все объекты теперь будут ссылаться на статический..
+        Citizen.nationality = "T.C";
+
+        System.out.println("Nationality " + Citizen.nationality);
+        System.out.println("Nationality of AliNuri " + citizen1.nationality);
+        System.out.println("Nationality of Mehmet" + citizen2.nationality);
+
+
+        Citizen.nationality = "USA";
+        citizen1.nationality = "USA";
+        citizen1.nationality = "USA";
+
+
+
+
+        //можно так вывести на экран
+        System.out.println("\nNationality " + Citizen.nationality);
+        System.out.println("Nationality of AliNuri " + citizen1.nationality);
+        System.out.println("Nationality of Mehmet" + citizen2.nationality);
+        //можно и так вывести на экран
+        System.out.println("\nNationality" + Citizen.getNationality());
+        System.out.println("Nationality of AliNuri " + citizen1.getNationality());
+        System.out.println("Nationality of Mehmet" + citizen2.getNationality());
+    }
+
+
+
+}
