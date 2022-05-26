8cfd7a9e4b32301b351b562fef0465b9865c0966
Ali Nuri <ansprogress@gmail.com>
2022-05-26 16:57:29 +0300

    Enum implementation v1.1

--- src/Ex_17_Enums/Date/DateTest.java
+++ src/Ex_17_Enums/Date/DateTest.java
@@ -11,6 +11,7 @@ import javax.xml.crypto.Data;
 public class DateTest {
     public static void main(String[] args){
 
+        // there is all given informations are impossible
         Date1 d1 = new Date1(-2, 25, 1232132132);
         Date2 d2 = new Date2("-2", "Ojak", "20022");
 
@@ -19,10 +20,8 @@ public class DateTest {
         Date1 d3 = new Date1(DateConstant.DAY_1, DateConstant.MARCH, DateConstant.YEAR_2022);
 
 
-
         //There we called enum from DateEnums class for Date4 class
         Date4 d4 = new Date4(DayOfMonth.DAY_1, Month.MARCH, Year.YEAR_2022);
-
 
     }
 }
